46b4ee993e8d454e2ade7d8a43bd37181006b796
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2022-08-30 11:41:34 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>React App</Text>
+    </View>
+  );
+};
+
+export default App;
