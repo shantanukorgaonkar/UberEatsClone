55e25714c758c0341616a9fdad0203d32af73ba7
Shantanu Korgaonkar <shantanukorgaonkar7@gmail.com>
2022-09-22 15:21:02 +0530

    ADD define api methods

--- api/index.js
+++ api/index.js
@@ -1,29 +1,124 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+const BASE_URL = "https://uber-eats-clone-backend.onrender.com"
+
+
 
 export const getOrders = async (city) => {
-    const url=`https://uber-eats-clone-backend.onrender.com/api/v1/orders`
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data.data
-    
-      } catch (error) {
-        console.log(error);
-      }
+  const url = `${BASE_URL}/api/v1/orders`
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.data
+
+  } catch (error) {
+    console.log(error);
+  }
 }
-      
 
 
-export const getRestaurants = async (city)=>{
-    const url=`https://uber-eats-clone-backend.onrender.com/api/v1/restaurants?city=${city}`
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-      
-        const restaurantData = data.data;
-        
-        return restaurantData
-      } catch (error) {
-        console.log(error);
+export const getRestaurants = async (city) => {
+  const url = `${BASE_URL}/api/v1/restaurants?city=${city}`
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const restaurantData = data.data;
+
+    return restaurantData
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const userSignUp = async (email, password) => {
+  const url = `${BASE_URL}/api/v1/users`
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  }
+
+  try {
+    const response = await fetch(url, options)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return error;
+  }
+}
+
+export const userLogIn = async (email, password) => {
+  const url = `${BASE_URL}/api/v1/users/login`
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  }
+  try {
+    const response = await fetch(url, options)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return error
+  }
+}
+
+export const getLoggedInUserInfo = async () => {
+  const url = `${BASE_URL}/api/v1/users/me`
+
+  try {
+    const jwtToken = await AsyncStorage.getItem("jwtToken");
+    if(jwtToken===null){
+      throw new Error("No jwt token present");
+    }
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${jwtToken}`
       }
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    return error
+  }
+}
+
+export const deleteUser = async ()=>{
+  const url = `${BASE_URL}/api/v1/users/me`
+
+try {
+  const jwtToken = await AsyncStorage.getItem("jwtToken");
+  if(jwtToken===null){
+    throw new Error("No jwt token present");
+  }
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${jwtToken}`
+    }
+  }
+  const response = await fetch(url, options)
+
+  const data = await response.json()
+  if(!data.data){
+    throw new Error("User Deletion Failed.");
+  }
+} catch (error) {
+  return error
+}
+
+
 }
